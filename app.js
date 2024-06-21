530481628ca955b143be6040a7d62d907b2b915a
Sudip Acharya <drxking456@gmail.com>
2024-06-21 20:40:46 +0545

    push it bro

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const hisaabModel = require("./models/hisaabModel")
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs")
 
 app.use(expressSession({
